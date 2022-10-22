e86ec4b60f42238dbb6b6b003b8ea16ffbff68de
Tyler Pak <tyler.j.pak@gmail.com>
2022-10-22 16:42:16 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tyler Pak on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
